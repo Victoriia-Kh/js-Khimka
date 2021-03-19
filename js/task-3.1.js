65b093968f85e9c5612587e4fca472a5c535e344
Victoriia-Kh <victoria.khimka@gmail.com>
2021-03-19 20:15:41 +0200

    hw3 task 1 - update

--- js/task-3.1.js
+++ js/task-3.1.js
@@ -17,43 +17,70 @@ var myCounter = 0
 
 function showPush() {
     myCounter ++;
+    console.log("К-ть зроблених дій = ", myCounter)
     arr.push(+myCounter);
+    console.log("Довжина масиву = ", arr.length)
 }
 function showUnshift(){
     myCounter ++;
+    console.log("К-ть зроблених дій = ", myCounter)
     arr.unshift(+myCounter);
+    console.log("Довжина масиву = ", arr.length)
 }
 function showPop(){
     myCounter ++;
+    console.log("К-ть зроблених дій = ", myCounter)
     arr.pop();
+    console.log("Довжина масиву = ", arr.length)
 }
 function showShift(){
     myCounter ++;
+    console.log("К-ть зроблених дій = ", myCounter)
     arr.shift();
+    console.log("Довжина масиву = ", arr.length)
 }
 
 function mainShow() {
-    dano = +prompt('Введіть число 1/2/3/4 і виконається відповідна дія з масивом. ' + '[ ' + arr + ' ]', '1/2/3/4');
+    dano = +prompt('Введіть число 1/2/3/4 і виконається відповідна дія з масивом. ' + "[ " + arr + " ]", '1/2/3/4');
+    return dano;
+}
+
+function secondShow() {
+    dano = +prompt('Введіть число 1/2 і виконається відповідна дія з масивом. ' + "Масив зараз пустий.", '1/2');
     return dano;
 }
 
 function mainFunc() {
-    dano = +mainShow();
-    switch(dano) {
-        case 1:
-            showPush();
-            break;
-        case 2:
-            showUnshift();
-            break;
-        case 3:
-            showPop();
-            break;
-        case 4:
-            showShift();
-            break;
-        default:
-            alert('Щось пішло не так (. Спробуйте ще раз ввести число.')
+    if (arr.length == 0) {
+        dano = +secondShow();
+        switch(dano) {
+            case 1:
+                showPush();
+                break;
+            case 2:
+                showUnshift();
+                break;
+            default:
+                alert('Щось пішло не так (. Спробуйте ще раз ввести число.')
+        }
+    } else {
+        dano = +mainShow();
+        switch(dano) {
+            case 1:
+                showPush();
+                break;
+            case 2:
+                showUnshift();
+                break;
+            case 3:
+                showPop();
+                break;
+            case 4:
+                showShift();
+                break;
+            default:
+                alert('Щось пішло не так (. Спробуйте ще раз ввести число.')
+        }
     }
 }
 
