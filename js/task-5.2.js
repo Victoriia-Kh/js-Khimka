2a497421e0dea9ba5c1a1289fac9eb55d4a7f6c9
Victoriia-Kh <victoria.khimka@gmail.com>
2021-04-08 20:36:47 +0300

    hw 5 changed

--- js/task-5.2.js
+++ js/task-5.2.js
@@ -3,13 +3,7 @@
     произведения и частного двух чисел.
     Протестировать данный объект, принимая от пользователя значения двух операндов и знак операции.
     В зависимости от принятого знака операции, вызвать соответствующий метод.
-*/
-
-var Calculator = {
-    num1 : 1,
-    num2 : 1,
-    sign : '+',
-    count() {
+        count() {
         switch(this.sign){
             case '+':
                 alert(this.num1 + this.num2)
@@ -24,11 +18,46 @@ var Calculator = {
                 alert(this.num1 / this.num2)
                 break
         }
+*/
+
+var Calculator = {
+    num1 : 1,
+    num2 : 1,
+    sign : '+',
+    plus : function() {
+        return this.num1 + this.num2;
+    },
+    minus : function() {
+        return this.num1 - this.num2;
+    },
+    mnoz : function() {
+        return this.num1 * this.num2;
+    },
+    dil : function() {
+        return this.num1 / this.num2;
     }
 }
 
- Calculator.num1 = +prompt('Введіть перше число', '123');
- Calculator.num2 = +prompt('Введіть друге число', '123');
- Calculator.sign = +prompt('Введіть знак операції', '+-*/');
+Calculator.num1 = +prompt('Введіть перше число', '123');
+Calculator.num2 = +prompt('Введіть друге число', '123');
+Calculator.sign = prompt('Введіть знак операції', '+-*/');
 
-Calculator.count()
+var a = 'Щось пішло не так'
+
+switch(Calculator.sign){
+    case '+':
+        a = Calculator.plus();
+        break
+    case '-':
+        a = Calculator.minus();
+        break
+    case '*':
+        a = Calculator.mnoz();
+        break
+    case '/':
+        a = Calculator.dil();
+        break
+    }
+
+
+alert(a)
